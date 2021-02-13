90af87451d43e8737aacab01b96123165f266ea6
Robert Bennett <robertbennett1998@outlook.com>
2021-02-13 18:37:47 +0000

    Finished outlined text box. Validation working and styling is working.

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   exports: [CreateAccountComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AccountModule { }
--- src/app/shared/text-box/text-box.component.ts
+++ src/app/shared/text-box/text-box.component.ts
@@ -1,38 +1,77 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, forwardRef, Input, OnInit, Optional, Self, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NgControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-text-box',
   templateUrl: './text-box.component.html',
   styleUrls: ['./text-box.component.css']
 })
-export class TextBoxComponent implements OnInit {
+export class TextBoxComponent implements OnInit, ControlValueAccessor {
+  @ViewChild('inputTextBox', {static: true}) inputTextBox!: ElementRef;
   @Input() label : string;
+  @Input() type : string;
+  
   isActive : boolean;
   isMouseOver : boolean;
+  hasError : boolean | null;
   hasValue : boolean;
+  onChangeCallback : any;
+  onTouched : any;
+
+  checkForError() {
+    this.hasError = this.control.invalid && this.control.touched;
+  }
 
   onInputFocused() {
     this.isActive = true;
+    this.checkForError();
   }
 
   onInputBlurred() {
     this.isActive = false;
+    this.onTouched();
+    this.checkForError();
   }
 
   onMouseOver() {
     this.isMouseOver = true;
+    this.checkForError();
   }
 
   onMouseOut() {
     this.isMouseOver = false;
+    this.checkForError();
   }
 
-  constructor() {
+  onChange(event : Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.hasValue = value.length > 0;
+    this.onChangeCallback(value);
+    this.checkForError();
+  }
+
+  constructor(@Self() @Optional() private control: NgControl) {
+    this.control = control;
+    this.control.valueAccessor = this;
     this.label = "";
     this.isActive = false;
     this.isMouseOver = false;
+    this.hasError = false;
     this.hasValue = false;
    }
+
+  writeValue(value: any): void {
+    this.inputTextBox.nativeElement.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {    
+    this.onTouched = fn;
+  }
 
   ngOnInit(): void {
   }
